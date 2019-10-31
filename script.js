ee9569a85f9f4cfb155d1016e9782dafae8962a9
wlodku <wlodku@gmail.com>
2019-10-31 11:23:43 +0100

    Refactor count function

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function drawGrid(){
     container.innerHTML = ""
     for (let i = 0; i < rows; i++) { 
         for (let j = 0; j < columns; j++)    { 
-            let cell = document.createElement("div");
+            let cell = document.createElement("div");            
             if(grid2[i][j]) {
                 cell.classList.add("alive");
             }
@@ -41,59 +41,22 @@ function drawGrid(){
 }
 
 // liczenie zywych sąsiadów
-function countLiveNeighbors(i, j){
-    let w, e, n, s;
-    let nw, sw, ne, se;
-    if (grid[j-1] != undefined) {
-        w = grid[i][j-1].classList.contains("alive") ? 1 : 0;
-    } else {
-        w = 0;
+function countLiveNeighbors(row, col) {
+    let count = 0;
+    for(let i = row - 1; i <= row + 1; i++) {
+        if (i >= 0 && i < rows){
+            for(let j = col - 1; j <= col + 1; j++) {
+                if (j >= 0 && j < columns){
+                    if (i != row || j != col) {
+                        count += grid[i][j].classList.contains("alive") ? 1 : 0;                        
+                    }                    
+                }                
+            }            
+        }            
     }
-
-    if(grid[j-1] != undefined && grid[i-1] != undefined) {
-        nw = grid[i-1][j-1].classList.contains("alive") ? 1 : 0;
-    } else {
-        nw = 0;
-    }
-
-    if (grid[j+1] != undefined) {
-        e = grid[i][j+1].classList.contains("alive") ? 1 : 0;
-    } else {
-        e = 0;
-    }
-
-    if(grid[i-1] != undefined && grid[j+1] != undefined) {
-        ne = grid[i-1][j+1].classList.contains("alive") ? 1 : 0;;
-    } else {
-        ne = 0;
-    }
-
-    if (grid[i-1] != undefined) {
-        n = grid[i-1][j].classList.contains("alive") ? 1 : 0;
-    } else {
-        n = 0;
-    }
-
-    if (grid[i+1] != undefined) {
-        s = grid[i+1][j].classList.contains("alive") ? 1 : 0;
-    } else {
-        s = 0;
-    }
-
-    if(grid[j+1] != undefined && grid[i+1] != undefined) {
-        se = grid[i+1][j+1].classList.contains("alive") ? 1 : 0;;
-    } else {
-        se = 0;
-    }
-
-    if(grid[j-1] != undefined && grid[i+1] != undefined) {
-        sw = grid[i+1][j-1].classList.contains("alive") ? 1 : 0;;
-    } else {
-        sw = 0;
-    }
-
-    return w + e + n + s + nw + ne + sw + se;
+    return count;
 }
+
 
 let go = document.getElementById("go");
 let go2 = document.getElementById("go2");
